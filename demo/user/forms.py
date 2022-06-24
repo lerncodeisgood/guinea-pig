27da3245ca3a87c5bc4b1b8746ae60314d04c30b
Nick <nick_lee@adata.com>
2022-06-24 11:40:46 +0800

    feat:using email as username

--- demo/user/forms.py
+++ demo/user/forms.py
@@ -2,10 +2,10 @@ from unittest.util import _MAX_LENGTH
 from django import forms
 
 class CreateUserPostForm(forms.Form):
-    username = forms.CharField(label='username',max_length=32,required=True)
+    
     email = forms.CharField(label='email',max_length=64,required=True)
-    mobile_phone = forms.CharField(label='mobile_phone',min_length=10,max_length=10,required=True)
     password = forms.CharField(label='password',max_length=32,required=True)
+    mobile_phone = forms.CharField(label='mobile_phone',min_length=10,max_length=10,required=False)
     country = forms.CharField(required=False, max_length=50)
     town = forms.CharField(required=False, max_length=20)
     address = forms.CharField(required=False, max_length=100)
